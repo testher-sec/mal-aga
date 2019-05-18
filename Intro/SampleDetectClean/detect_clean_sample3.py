65cd8474c7b9fbc3a7a618949114c4d370932b1e
Neska Sec <testher.sec@gmail.com>
2019-05-18 20:13:20 +0200

    sample3 remove first 840h bytes

--- Intro/SampleDetectClean/detect_clean_sample3.py
+++ Intro/SampleDetectClean/detect_clean_sample3.py
@@ -23,4 +23,8 @@ class Sample3Analyzer(Analyzer):
 
     def clean(self, file_name):
         print 'sample3. file name to clean... ', file_name
+        output_file_name = 'cleant-' + file_name
+        with open(file_name, 'rb') as in_file:
+            with open(output_file_name, 'wb') as out_file:
+                out_file.write(in_file.read()[847:])
         return True
